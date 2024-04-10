07aa8815f626963bef4cfa9507bf3443669275f9
Polokego <pomakga022@student.wethinkcode.co.za>
2024-04-10 23:09:41 +0200

    Completed dropzone evemt

--- bank_analytic_dashboard/src/app/loader/loader.component.ts
+++ bank_analytic_dashboard/src/app/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./loader.component.css']
 })
 export class LoaderComponent {
+  csvContent!: string;
+  counter:any = 0;
+  constructor(){
+
+
+  }
+
+  //@HostListener('drop',['$event'])
+  onDrop(event:any): void{
+    event.preventDefault();      
+    console.log(event.dataTransfer.files);
+
+    alert("dropped file")
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      console.log("WORKING");
+
+      const fileName = files[0].name;
+      console.log(`Selected file name: ${fileName}`);
+    }
+  }
+
+  onDragOver(event:any): void{
+    this.counter+= 1;
+    //console.log(this.counter)
+    event.preventDefault();      
+   
+  }
+
+
+  async onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+        alert("successful")
+    }
+
+  }
 
 }
